043a56eda7897a27877c80871251f3682becf4ad
smanojpravin <smanojpravin@gmail.com>
2022-11-03 14:05:48 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
